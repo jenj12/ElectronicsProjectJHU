3da95b826946bc8c800b7a7e8687e0ca6bcf0e09
jenj12 <jennif12@ualberta.ca>
2022-07-26 11:56:26 -0600

    now resets timer if screen has been off for longer than break time.

--- code.py
+++ code.py
@@ -5,6 +5,7 @@ timer2_on = False
 max_screen_time = 1200    #in seconds
 break_time = 20     #in seconds
 break_started = False
+screen_off = False
 def notify():
     cp.pixels.fill((50, 0, 0)) #need to add specific instructions: turn led on, play sound
     cp.play_tone(260, 0.5)
@@ -20,17 +21,27 @@ while True:
     #if screen is on and timer is off
     if light > threshold:
         if break_started == False and timer1_on == False :
-        #turn on timer
             timer1_on = True
             #take start time
             initial_time = time.time()
             print("timer on")
             print(initial_time)
 
+    if screen_off == True:
+        t_elapsed = time.time() - time_screen_off
+        if t_elapsed > break_time:
+            # print("screen has been off for a while")
+            if light >= threshold:
+                print("restarting timer")
+                initial_time = time.time()
+                print(initial_time)
+                screen_off = False
+
     #if the timer is on
     if timer1_on == True:
         #if screen is on
         if light >= threshold:
+            screen_off = False
             #calculate time elapsed
             time_elapsed = time.time() - initial_time
             #if time elapsed > max screen time
@@ -40,9 +51,12 @@ while True:
                 timer1_on = False
                 break_started = True
                 print("break started")
+        elif light < threshold and screen_off == False:
+            screen_off = True
+            time_screen_off = time.time()            
         
     if timer2_on == False and break_started == True:   
-        if light < threshold:
+        if light <= threshold:
             print("start rest")
             off()
             #start second timer
@@ -60,12 +74,14 @@ while True:
             print("go back to rest!")
             #turn on LEDs and alarm again
             notify()
+            time_screen_off = time.time()
         else:
             off()
             t_elapsed = time.time() - time_screen_off
-            #if time elapsed > 5min ish (if the screen has been off for a while)
+            #if time elapsed > 20 seconds (if the screen has been off for a while)
             if t_elapsed > break_time:
                 print("rest over")
                 #turn off timer
                 timer2_on = False
                 break_started = False
+                cp.play_tone(440, 0.5)
